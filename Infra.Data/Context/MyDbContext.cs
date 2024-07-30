8d11c8ae0568a04e3e1df87843ed6ef07f04d572
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2024-07-30 09:55:39 -0300

    Adicionando Primeirra Tabela de Teste  , Ligaçao Banco com Projeto

---
+++ Infra.Data/Context/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Context
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { 
+        
+        }
+
+
+        public DbSet<Users> Users { get; set; }
+
+
+    }
+}
---
+++ Infra.Data/EntitiesConfiguration/MyEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.EntitiesConfiguration
+{
+    public class MyEntityConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(255);
+            builder.Property(x => x.Email).HasMaxLength(255);
+            builder.Property(x => x.Password).HasMaxLength(255);
+        }
+    }
+}
